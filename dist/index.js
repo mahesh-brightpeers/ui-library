64c32ace493a39c45ba9f09f2aaff5ea4b951d7d
Mahesh B. <mahesh.bharadva@tgstechnology.com>
2026-01-31 16:37:14 +0530

    build created

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
 export * from './provider/app-config-provider';
 export * from './components/layout/admin-header/admin-header.component';
+export * from './components/layout/admin-layout/admin-sidebar.component';
